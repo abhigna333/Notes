2415beab5afd309cbf4f7ddb46647259f88455ea
abhigna333 <abhignareddy333@gmail.com>
2022-05-29 19:18:34 +0530

    Added Authenticate UI

--- lib/pages/faceidpage.dart
+++ lib/pages/faceidpage.dart
@@ -3,6 +3,7 @@ import 'package:app_lock_face/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FaceIdPage extends StatefulWidget {
   const FaceIdPage({Key? key}) : super(key: key);
@@ -16,19 +17,49 @@ class _FaceIdPageState extends State<FaceIdPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton.icon(
-              onPressed: () async{
-                final isAuth = await LocalAuthApi.authenticate();
-                if (isAuth){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: ((context) => HomePage())),);
-                }
-              }, 
-              icon: Icon(
-                Icons.lock_open,
-              ), 
-              label: Text("Authenticate"),),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Heyy There!",
+              style: GoogleFonts.nunito(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10.0,),
+
+            Text(
+              "Let's get your notes"
+            ),
+
+            SizedBox(height: 30.0,),
+
+            Icon(Icons.face_outlined, size: 75,),
+
+            SizedBox(height: 30.0,),
+
+            ElevatedButton.icon(
+                  onPressed: () async{
+                    final isAuth = await LocalAuthApi.authenticate();
+                    if (isAuth){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder: ((context) => HomePage())),);
+                    }
+                  }, 
+                  icon: Icon(
+                    Icons.lock_open,
+                  ), 
+                  label: Text("Authenticate"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 21, 137, 214),
+                  ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
